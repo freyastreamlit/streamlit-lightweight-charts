dd928f311fa9139687a110835d804b8653032258
Joe Rosa <joe.rosa@itpmngt.co.uk>
2023-03-24 17:03:41 +0000

    Example using Pandas (release 0.7.15)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="streamlit-lightweight-charts",
-    version="0.7.14",
+    version="0.7.15",
     author="Joe Rosa",
     author_email="joe.rosa@itpmngt.co.uk",
     license="MIT",
